f6f6426c8d53358fb20a40721522da7670cf5551
Bram Meijer <brammeijer12@gmail.com>
2024-12-04 16:38:55 +0100

    day 4, part two

--- README.md
+++ README.md
@@ -9,10 +9,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `123.9µs` | `220.1µs` |
-| [Day 2](./src/bin/02.rs) | `83.3µs` | `173.4µs` |
-| [Day 3](./src/bin/03.rs) | `100.9µs` | `181.8µs` |
-| [Day 4](./src/bin/04.rs) | `767.3µs` | `-` |
+| [Day 1](./src/bin/01.rs) | `58.2µs` | `108.9µs` |
+| [Day 2](./src/bin/02.rs) | `88.5µs` | `170.8µs` |
+| [Day 3](./src/bin/03.rs) | `98.9µs` | `175.8µs` |
+| [Day 4](./src/bin/04.rs) | `762.8µs` | `332.7µs` |
 
-**Total: 1.65ms**
+**Total: 1.80ms**
 <!--- benchmarking table --->
--- src/bin/04.rs
+++ src/bin/04.rs
@@ -2,6 +2,7 @@ advent_of_code::solution!(4);
 
 use diagonal::diagonal_pos_neg;
 use diagonal::diagonal_pos_pos;
+use grid::Grid;
 
 pub fn check_chars(input: &[char]) -> bool {
     let xmas_bool = input[0] == 'X' && input[1] == 'M' && input[2] == 'A' && input[3] == 'S';
@@ -51,8 +52,40 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(total_xmas_count)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut levels: Vec<Vec<char>> = vec![];
+    let lines = input.split('\n');
+    lines
+        .into_iter()
+        .filter(|&x| !x.is_empty())
+        .for_each(|f: &str| {
+            levels.push(f.chars().collect::<Vec<_>>());
+        });
+
+    let grid_input = Grid::from(&levels);
+    let a = grid_input
+        .indexed_iter()
+        .filter(|((x, y), &t)| {
+            let mut x1 = false;
+            let mut x2 = false;
+            if *x > 0 && *y > 0 {
+                x1 = {
+                    grid_input.get(x + 1, y + 1).is_some_and(|&f| f == 'M')
+                        && grid_input.get(x - 1, y - 1).is_some_and(|&f| f == 'S')
+                        || grid_input.get(x + 1, y + 1).is_some_and(|&f| f == 'S')
+                            && grid_input.get(x - 1, y - 1).is_some_and(|&f| f == 'M')
+                };
+                x2 = {
+                    grid_input.get(x + 1, y - 1).is_some_and(|&f| f == 'M')
+                        && grid_input.get(x - 1, y + 1).is_some_and(|&f| f == 'S')
+                        || grid_input.get(x + 1, y - 1).is_some_and(|&f| f == 'S')
+                            && grid_input.get(x - 1, y + 1).is_some_and(|&f| f == 'M')
+                };
+            }
+            t == 'A' && x1 && x2
+        })
+        .count() as u32;
+    Some(a)
 }
 
 #[cfg(test)]
@@ -68,6 +101,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
