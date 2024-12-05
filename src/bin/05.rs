a3b1279cdf5c4923a5d389591616a7fee2ac0f73
Bram Meijer <brammeijer12@gmail.com>
2024-12-05 13:04:25 +0100

    day 5, part two

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 advent_of_code::solution!(5);
 
 #[derive(Debug)]
@@ -51,7 +53,58 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines = input.split('\n');
+    let mut rules: Vec<Rule> = vec![];
+    let mut updates: Vec<Vec<u32>> = vec![];
+
+    //parsing
+    for line in lines {
+        if line.find('|').is_some() {
+            let values: Vec<&str> = line.split("|").collect();
+            if values.len() == 2 {
+                rules.push(Rule {
+                    before: values.get(0)?.parse::<u32>().unwrap(),
+                    after: values.get(1)?.parse::<u32>().unwrap(),
+                });
+            }
+        } else if !line.is_empty() {
+            let values: Vec<u32> = line.split(",").map(|f| f.parse::<u32>().unwrap()).collect();
+            updates.push(values);
+        }
+    }
+    //solving
+    let mut correct_update_total: u32 = 0;
+    for update in updates {
+        let mut tmp_vec: Vec<u32> = vec![];
+        let mut fixed_update = false;
+        for &item in &update {
+            tmp_vec.push(item);
+            rules.iter().filter(|f| f.before == item).for_each(|r| {
+                match tmp_vec.iter().position(|&f| f == r.after) {
+                    Some(index) => {
+                        //rule is present in this update, shift item to right posi
+                        match tmp_vec.iter().position(|&f| f == item) {
+                            Some(current_position) => {
+                                if current_position > index {
+                                    //if item is present and too far too the right, move towards correct index.
+                                    tmp_vec.remove(current_position);
+                                    tmp_vec.insert(index, item);
+                                    fixed_update = true;
+                                }
+                            }
+                            None => {}
+                        };
+                    }
+                    None => {}
+                }
+            });
+        }
+        if fixed_update {
+            let index = (tmp_vec.len() as f32 / 2 as f32).floor() as usize;
+            correct_update_total += tmp_vec[index];
+        }
+    }
+    Some(correct_update_total)
 }
 
 #[cfg(test)]
@@ -67,6 +120,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
